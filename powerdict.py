82fb0cef2de5c9c87cb38129cacdf21af54056d7
ssalteko <ssalteko@gmail.com>
2015-09-06 22:46:18 -0700

    Create powerdict.py
    this raises each dict key to a power of its value

---
+++ powerdict.py
@@ -0,0 +1,8 @@
+#this raises each dict key to a power of its value
+
+def powerdict(n):
+    '''This raises each dict key to a power of its value'''
+    y = 1
+    for x in k:
+        y *= x**k[x]
+    return y
